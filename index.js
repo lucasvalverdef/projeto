ba66bcf24546767fe42977aec6122e0cf21fba74
Lucas Valverde <lucasferreiravalverde@gmail.com>
2024-09-11 15:30:50 -0300

    criação e organização dos .css e .js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require("body-parser");
+const path = require('path'); // Adicionar o path para manipulação de caminhos
 const Post = require('./models/Post');
 
 // Configuração
@@ -14,12 +15,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Conectar CSS
-app.use(express.static('public'));
-app.use(express.static('files'))
+// Servir a pasta 'public' onde está o CSS
+app.use(express.static(path.join(__dirname, 'public'))); // Apenas uma vez
 
 // Rotas
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/PRINCIPAL/principal.html");
+    res.sendFile(path.join(__dirname, 'principal', 'principal.html')); // Usar path.join para segurança e compatibilidade
 })
 
 app.post('/add', function (req, res) {
@@ -27,15 +28,14 @@ app.post('/add', function (req, res) {
         nomeC: req.body.nomeC,
         emailC: req.body.emailC,
         foneC: req.body.foneC,
-        
-
-    }).then(function(){
-        res.sendFile(__dirname + "/PRINCIPAL/principal.html");
-      }).catch(function(erro){
-        res.send("Erro localizado: "+erro)
-      })
+    }).then(function () {
+        res.sendFile(path.join(__dirname, 'principal', 'principal.html')); // Enviar novamente o arquivo HTML após o sucesso
+    }).catch(function (erro) {
+        res.send("Erro localizado: " + erro); // Exibir erro
+    });
 })
 
+// Iniciar o servidor
 app.listen(8081, function () {
     console.log("Servidor rodando na url http://localhost:8081");
 });
